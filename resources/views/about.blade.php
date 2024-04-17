eefc1f5d9adf996a0f5f4a57eb174fcad93db4e8
Storm <stormurbjorn24@gmail.com>
2024-04-17 14:05:23 +0700

    first commit

---
+++ resources/views/about.blade.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>New Page</title>
+    <link rel="stylesheet" href="{{ asset('css/style.css') }}">
+</head>
+<body>
+    <h1>Hello, this is a new page!</h1>
+    <p>{{ $data }}</p>
+    <script src="{{ asset('js/script.js') }}"></script>
+</body>
+</html>
